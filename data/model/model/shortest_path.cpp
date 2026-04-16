bdb41ab64071d5978e01c305552f1c8feab75ede
Lyke De Leon <deleonlyke006@gmail.com>
2026-04-16 23:24:27 +0800

    Create shortest_path.cpp

---
+++ data/model/model/shortest_path.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Shortest Path:\n";
+    cout << "A → B → D → E → M → N → O → U → V → E1 → F1\n";
+
+    cout << "Total Distance: 947.74 meters\n";
+    cout << "Travel Time: ~3.79 minutes\n";
+    cout << "Round Trip: ~8 minutes\n";
+
+    return 0;
+}
