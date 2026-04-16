5c75a87ea932f3e3b7d5beb74be1c84057123f9e
Lyke De Leon <deleonlyke006@gmail.com>
2026-04-16 23:24:11 +0800

    Create max_flow.cpp

---
+++ data/model/max_flow.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int minCapacity = 16;
+    int maxCapacity = 19;
+
+    cout << "Evacuation Capacity per Trip: "
+         << minCapacity << " to " << maxCapacity << " persons" << endl;
+
+    cout << "This represents the MAX FLOW of the system." << endl;
+
+    return 0;
+}
